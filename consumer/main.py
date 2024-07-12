f9939ae4ec0b9d22630f50f7af68e400dbb82876
Stuzanna <smould1011@gmail.com>
2024-07-12 08:55:34 +0100

    tidy consumer, exceptions, try SR

--- consumer/main.py
+++ consumer/main.py
@@ -1,6 +1,9 @@
 import json
 import sys
-from confluent_kafka import DeserializingConsumer
+
+from confluent_kafka import DeserializingConsumer, KafkaError, KafkaException
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroDeserializer
 
 # -- Config --
 
@@ -8,16 +11,22 @@ bootstrap_servers = "localhost:19092"
 topics = ["customers"]
 client_id = "myClientId"
 consumer_group = "stu-consumer-group"
-schema_registry_url = "http://localhost:18081" 
+schema_registry_url = "http://localhost:18081"
 offset_config = "earliest"
 
-# -- Creating the Consumer ---
+# -- Schema Registry Client ---
+schema_registry_conf = {'url': schema_registry_url}
+schema_registry_client = SchemaRegistryClient(schema_registry_conf)
 
+# --- Creating the Consumer ---
+#   --- Serializers ---
 def json_deserializer(msg, s_obj=None):
     # return json.loads(msg.decode('ascii'))
     return json.loads(msg)
-def avro_deserializer(msg)
 
+avro_deserializer = AvroDeserializer(schema_registry_client)
+
+#  --- Config ---
 conf = {
     'bootstrap.servers': bootstrap_servers,
     'client.id': client_id,
@@ -26,9 +35,11 @@ conf = {
     # 'ssl.ca.location': '../sslcerts/ca.pem',
     # 'ssl.certificate.location': '../sslcerts/service.cert',
     # 'ssl.key.location': '../sslcerts/service.key', 
-    # 'key.deserializer': json_deserializer, # if key in JSON
+    'key.deserializer': json_deserializer, # if key in JSON use these two
     'value.deserializer': json_deserializer,
-    'schema.registry.url': schema_registry_url,
+    # 'key.deserializer': avro_deserializer, # if key in avro use these two
+    # 'value.deserializer': avro_deserializer,
+    # 'schema.registry.url': schema_registry_url,
     'auto.offset.reset': offset_config,
     }
 
@@ -57,9 +68,15 @@ try:
             elif msg.error():
                 raise KafkaException(msg.error())
         else:
-             print(f"{msg.partition()}:{msg.offset()}: "
-                  f"k={msg.key()} "
-                  f"v={msg.value()}")
+            try:
+                value = msg.value()
+                key = msg.key()
+                print(f"{msg.partition()}:{msg.offset()}: "
+                      f"k={msg.key()} "
+                      f"v={msg.value()}")
+            except Exception as e:
+                print(f"Message deserialization failed: {e}")
+                continue
 finally:
     # Close down consumer to commit final offsets.
     consumer.close()    
