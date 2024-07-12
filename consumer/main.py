8d188e240c8aa998fb1a26cd0c58790cf999bcf1
Stuzanna <smould1011@gmail.com>
2024-07-12 08:11:36 +0100

    correct json to deserialiser, add SR value

--- consumer/main.py
+++ consumer/main.py
@@ -8,13 +8,15 @@ bootstrap_servers = "localhost:19092"
 topics = ["customers"]
 client_id = "myClientId"
 consumer_group = "stu-consumer-group"
+schema_registry_url = "http://localhost:18081" 
 offset_config = "earliest"
 
 # -- Creating the Consumer ---
 
-def json_serializer(msg, s_obj=None):
+def json_deserializer(msg, s_obj=None):
     # return json.loads(msg.decode('ascii'))
     return json.loads(msg)
+def avro_deserializer(msg)
 
 conf = {
     'bootstrap.servers': bootstrap_servers,
@@ -24,8 +26,9 @@ conf = {
     # 'ssl.ca.location': '../sslcerts/ca.pem',
     # 'ssl.certificate.location': '../sslcerts/service.cert',
     # 'ssl.key.location': '../sslcerts/service.key', 
-    # 'key.deserializer': json_serializer, # if key in JSON
-    'value.deserializer': json_serializer,
+    # 'key.deserializer': json_deserializer, # if key in JSON
+    'value.deserializer': json_deserializer,
+    'schema.registry.url': schema_registry_url,
     'auto.offset.reset': offset_config,
     }
 
